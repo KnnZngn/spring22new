71adfd74554c67a942174a2d3fd6c2b938b62567
Kenan Zengin <k.zezgn@gmail.com>
2022-07-16 20:59:00 +0300

    16 july 22

---
+++ src/day29/E06.java
@@ -0,0 +1,16 @@
+package day29;
+
+public class E06 {
+/*
+                        Java is Object Oriented Programming Language
+                        Object Oriented Programming Language has four principals
+                        1)Encapsulation
+                        2)Polymorphism
+                        3)Abstraction
+                        4)Inheritance
+
+
+*/
+
+
+}
